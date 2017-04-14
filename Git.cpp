daeb5bcc3ff84fad42ddca5670343780de5fd95c
NightX123 <ayomideazeez43@gmail.com>
2017-04-14 13:27:56 +0100

    lol

--- Git.cpp
+++ Git.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+int function(int x);
 
 int main()
 {
-	cout<<"Hello World";
-	cout<<"Hiiiiii";
+	int num1;
+	cin>>num1;
+	cout<<function(num1);
+}
+int function(int x)
+{
+	if(x==1)
+	{
+		return x;
+	}
+	else
+	{
+		
+		return x*function(x-1);
+	}
 }
